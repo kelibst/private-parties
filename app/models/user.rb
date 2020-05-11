4cfc01ba97d347566279ba309f39336c3a45ab66
kelibst <kbooster17@gmail.com>
2020-05-11 15:53:47 +0000

    added user validations and sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :name, presence: true, uniqueness: true, length: {in: 3..20}
+    validates :username, presence: true, uniqueness: true, length: {in: 3..20}
 end
