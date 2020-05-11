e32bf1b0eb256ce49a265484d154f5288cbe5510
kelibst <kbooster17@gmail.com>
2020-05-11 16:45:42 +0000

    added login authentications

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'homepage#home'
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:create, :show]
+  get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
