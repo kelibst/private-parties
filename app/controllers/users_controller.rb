e2333fedaa4fa30df2a1ac8658ac8174fdefff25
kelibst <kbooster17@gmail.com>
2020-05-11 16:08:06 +0000

    added style to alerts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-        params.require(:user).permit(:username, :email, :password)
+        params.require(:user).permit(:username, :name)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    validates :name, presence: true, uniqueness: true, length: {in: 3..20}
+    validates :name, presence: true, uniqueness: true, length: {in: 3..100}
     validates :username, presence: true, uniqueness: true, length: {in: 3..20}
 end
