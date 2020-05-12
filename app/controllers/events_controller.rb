6c8abfcf4c100829681481c0fda77a5b9751e7b7
kelibst <kbooster17@gmail.com>
2020-05-12 08:04:53 +0000

    milestone 2 done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+    before_action :set_event, only: [:show, :edit]
+
     def new
         @event = Event.new
     end
@@ -14,12 +16,27 @@ class EventsController < ApplicationController
         end
     end
 
+    def index
+        @events = Event.all
+    end
+
     def show
+        @user = current_user
+    end
+
+    def edit
+    end
+
+    def destroy
     end
 
     private
 
     def event_params
         params.require('event').permit(:description, :title, :date, :location)
+    end
+
+    def set_event
+        @event = Event.find(params[:id])
     end
 end
